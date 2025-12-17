960becbecf8fff11b3f16d91b0086b7f0e9d5f17
SvenLee-J <sjansengorreta@cifpfbmoll.eu>
2025-12-17 05:09:42 +0100

    Actualitzacio Tests

--- src/test/java/eu/cifpfbmoll/FibonacciTest.java
+++ src/test/java/eu/cifpfbmoll/FibonacciTest.java
@@ -14,11 +14,49 @@ class FibonacciTest {
     private Fibonacci fibonacci = new Fibonacci();
 
     @Test
-    void fibonacciOfNegativeThrowsException() {
-        assertThrows(IllegalArgumentException.class, () -> fibonacci.calculate(-1));
+    void fibonacciOfZeroIsZero() {
+        assertEquals(0, fibonacci.calculate(0));
     }
 
+    @Test
+    void fibonacciOfOneIsOne() {
+        assertEquals(1, fibonacci.calculate(1));
+    }
 
+    @Test
+    void fibonacciOfTwoIsOne() {
+        assertEquals(1, fibonacci.calculate(2));
+    }
+
+    @Test
+    void fibonacciOfThreeIsTwo() {
+        assertEquals(2, fibonacci.calculate(3));
+    }
+
+    @Test
+    void fibonacciOfFourIsThree() {
+        assertEquals(3, fibonacci.calculate(4));
+    }
+
+    @Test
+    void fibonacciOfFiveIsFive() {
+        assertEquals(5, fibonacci.calculate(5));
+    }
+
+    @Test
+    void fibonacciOfSixIsEight() {
+        assertEquals(8, fibonacci.calculate(6));
+    }
+
+    @Test
+    void fibonacciOfSevenIsThirteen() {
+        assertEquals(13, fibonacci.calculate(7));
+    }
+
+    @Test
+    void fibonacciOfEightIsTwentyOne() {
+        assertEquals(21, fibonacci.calculate(8));
+    }
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 5, 10, 20})
